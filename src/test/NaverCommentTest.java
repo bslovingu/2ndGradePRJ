f180bec6c3428e89b5af8b71777946c877334be6
bslovingu <sincethe1997@naver.com>
2020-06-08 09:30:20 +0900

    본문 -> 일반 뉴스 / 스포츠 / 연예 구분해서 크롤링 댓글 -> 일반 뉴스에서만 (연예 쪽 댓글 없음)

--- src/test/NaverCommentTest.java
+++ src/test/NaverCommentTest.java
@@ -4,12 +4,17 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+import poly.util.CmmUtil;
 
 public class NaverCommentTest {
 
@@ -28,118 +33,127 @@ public class NaverCommentTest {
 
 		String url = "https://news.naver.com/main/read.nhn?m_view=1&includeAllCount=true&mode=LSD&mid=sec&sid1=102&oid=028&aid=0002498741";
 
-		int tmpIndex = url.lastIndexOf("&");
-		/* System.out.println(tmpIndex); // 현재는 값이 100 */
-		
-		String oid = url.substring(tmpIndex-3, tmpIndex);
-		String aid = url.substring(tmpIndex+5);
-		
-		System.out.println(oid);
-		System.out.println(aid);
-		
-		String page = "2"; // 페이지 수를 댓글 개수 넘긴거로 ( /20)+1 설정, 크롤링 반복 
+		if (url.charAt(8) == 'n') {
+			int tmpIndex = url.lastIndexOf("&");
+			/* System.out.println(tmpIndex); // 현재는 값이 100 */
 
-		String r_url = "https://apis.naver.com/commentBox/cbox/web_naver_list_jsonp.json?ticket=news&templateId=default_society&pool=cbox5&_callback=jQuery1706087132582402561_1534112016030&lang=ko&country=&objectId=news"
-				+ oid + "%2C" + aid
-				+ "&categoryId=&pageSize=20&indexSize=10&groupId=&listType=OBJECT&pageType=more&page=" + page;
+			String oid = url.substring(tmpIndex - 3, tmpIndex);
+			String aid = url.substring(tmpIndex + 5);
 
-		/*
-		 * Connection.Response response =
-		 * Jsoup.connect(url).method(Connection.Method.GET).execute(); Document doc =
-		 * response.parse();
-		 */
+			System.out.println(oid);
+			System.out.println(aid);
 
-		URL obj = new URL(r_url);
+			String test_url = "https://apis.naver.com/commentBox/cbox/web_naver_list_jsonp.json?ticket=news&templateId=default_society&pool=cbox5&_callback=jQuery1706087132582402561_1534112016030&lang=ko&country=&objectId=news"
+					+ oid + "%2C" + aid
+					+ "&categoryId=&pageSize=20&indexSize=10&groupId=&listType=OBJECT&pageType=more&page=";
+			int cnt = 1;
+			boolean testb = true;
+			List<String> strList = new ArrayList<String>();
+			while (testb) {
+				String t_url = test_url + Integer.toString(cnt);
+				URL obj = new URL(t_url);
 
-		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+				HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
-		// add request header
-		con.setRequestMethod("POST");
-		con.setRequestProperty("User-Agent", USER_AGENT);
-		con.setRequestProperty("Content-Type", "application/json");
-		con.setRequestProperty("referer", url);
+				// add request header
+				con.setRequestMethod("POST");
+				con.setRequestProperty("User-Agent", USER_AGENT);
+				/* con.setRequestProperty("Content-Type", "application/json"); */
+				con.setRequestProperty("referer", url);
 
-		// Send post request
-		con.setDoOutput(true);
+				// Send post request
+				con.setDoOutput(true);
 
-		System.out.println("con : " + con);
-		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		wr.flush();
-		wr.close();
+				DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+				wr.flush();
+				wr.close();
 
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
+				int responseCode = con.getResponseCode();
 
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
+				BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				String inputLine;
+				StringBuffer response = new StringBuffer();
 
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+				in.close();
 
-		// print result
+				// print result
 
-		String tmpStr = response.toString();
-		int openIndex = tmpStr.indexOf("(");
-		int closeIndex = tmpStr.lastIndexOf(")");
+				String tmpStr = response.toString();
+				int openIndex = tmpStr.indexOf("(");
+				int closeIndex = tmpStr.lastIndexOf(")");
 
-		System.out.println("--------------");
+				String str = tmpStr.substring(openIndex + 1, closeIndex);
 
-		System.out.println(openIndex);
-		System.out.println(closeIndex);
+				/*
+				 * JsonParser parser = new JsonParser(); Object tmpObj = parser.parse(str);
+				 * JsonObject jsonObj = (JsonObject) tmpObj;
+				 */
 
-		String str = tmpStr.substring(openIndex + 1, closeIndex);
+				/*
+				 * JSONParser jParser = new JSONParser();
+				 * 
+				 * JSONArray jobj0 = (JSONArray) jParser.parse(str);
+				 * 
+				 * for (int i = 0; i < jobj0.size(); i++) { JSONObject tmp = (JSONObject)
+				 * jobj0.get(i); String comment = String.valueOf(tmp.get("result"));
+				 * 
+				 * }
+				 */
 
-		System.out.println(str);
+				JsonParser jsonParser = new JsonParser();
+				JsonElement jsonElement = jsonParser.parse(str);
 
-		/*
-		 * JsonParser parser = new JsonParser(); Object tmpObj = parser.parse(str);
-		 * JsonObject jsonObj = (JsonObject) tmpObj;
-		 */
+				/*
+				 * String result1 = jsonElement.getAsJsonObject().get("result").toString();
+				 * System.out.println(result1);
+				 * 
+				 * System.out.println("-----------------------"); String result2 =
+				 * jsonElement.getAsJsonObject().get("result").getAsJsonObject().toString();
+				 * System.out.println(result2);
+				 */
 
-		/*
-		 * JSONParser jParser = new JSONParser();
-		 * 
-		 * JSONArray jobj0 = (JSONArray) jParser.parse(str);
-		 * 
-		 * for (int i = 0; i < jobj0.size(); i++) { JSONObject tmp = (JSONObject)
-		 * jobj0.get(i); String comment = String.valueOf(tmp.get("result"));
-		 * 
-		 * }
-		 */
+				String result3 = jsonElement.getAsJsonObject().get("result").getAsJsonObject().get("commentList")
+						.toString();
 
-		JsonParser jsonParser = new JsonParser();
-		JsonElement jsonElement = jsonParser.parse(str);
+				JsonElement finalElement = jsonParser.parse(result3);
 
-		/*
-		 * String result1 = jsonElement.getAsJsonObject().get("result").toString();
-		 * System.out.println(result1);
-		 * 
-		 * System.out.println("-----------------------"); String result2 =
-		 * jsonElement.getAsJsonObject().get("result").getAsJsonObject().toString();
-		 * System.out.println(result2);
-		 */
+				// String result4 =
+				// finalElement.getAsJsonArray().get(0).getAsJsonObject().get("contents").toString();
+				// Array 인덱스는 0~19로 가져오면 됨
 
-		System.out.println("-----------------------");
-		String result3 = jsonElement.getAsJsonObject().get("result").getAsJsonObject().get("commentList").toString();
-		System.out.println(result3);
+				int k = 0;
+				Iterator<JsonElement> iter = jsonElement.getAsJsonObject().get("result").getAsJsonObject()
+						.get("commentList").getAsJsonArray().iterator();
+				while (iter.hasNext()) {
+					k++;
+					iter.next();
+				}
+				String[] arr = new String[k];
 
-		JsonElement finalElement = jsonParser.parse(result3);
+				for (int i = 0; i < k; i++) {
+					arr[i] = CmmUtil.nvl(jsonElement.getAsJsonObject().get("result").getAsJsonObject()
+							.get("commentList").getAsJsonArray().get(i).getAsJsonObject().get("contents").toString());
+					strList.add(arr[i]);
+					if (k != 20 && i == k - 1) {
+						testb = false;
+					}
+				}
+				cnt++;
+			}
 
-		System.out.println("-----------------------");
-		String result4 = finalElement.getAsJsonArray().get(0).getAsJsonObject().get("contents").toString();
-		// Array 인덱스는 0~19로 가져오면 됨
-		System.out.println(result4);
+			System.out.println("-----------------------------------");
 
-		System.out.println("-----------------------------------");
-		String[] arr = new String[20];
-		for (int i = 0; i < 20; i++) {
-			arr[i] = jsonElement.getAsJsonObject().get("result").getAsJsonObject().get("commentList").getAsJsonArray()
-					.get(i).getAsJsonObject().get("contents").toString();
-			System.out.println(arr[i]);
+			System.out.println(strList.size());
+
+			/*
+			 * for(int i =0; i<strList.size(); i++) { System.out.println(strList.get(i)); }
+			 */
+
+			System.out.println(strList.get(100));
+
 		}
 
 	}
--- src/test/NaverContentTest.java
+++ src/test/NaverContentTest.java
@@ -13,29 +13,72 @@ public class NaverContentTest {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		String add = "m_view=1&includeAllCount=ture&";
-		String url = "https://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=028&aid=0002498741";
+		char s = 's';
+		char e = 'e';
+		char n = 'n';
+
+		String add = "m_view=1&includeAllCount=true&";
+		// String url =
+		// "https://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=028&aid=0002498741";
+		/*
+		 * String url ="https://entertain.naver.com/read?oid=416&aid=0000258951"; // 연예
+		 */
+
+		String url = "https://entertain.naver.com/read?oid=109&aid=0004220414"; // 스포츠
+
+		int tmp = url.indexOf("?");
+		System.out.println(tmp);
+
+		String url_1 = url.substring(0, tmp + 1);
+		String url_2 = url.substring(tmp + 1);
+		String commURL = url_1 + add + url_2;
+		System.out.println(commURL);
+
 		// 경주를 검색했을 때 가정, 첫 번째 뉴스를 예시로 연습
 		// 한가지 키워드에 대해서 뉴스를 반복문으로 10개 크롤링
+		// add를 ? 뒤에 추가해서 댓글 쪽 url NaverCommentTest로 넘기기
 
 		Document doc = null;
 		doc = Jsoup.connect(url).get();
 
-		Elements conElement = doc.select("div#main_content");
-
-		Iterator<Element> content = conElement.select("div#articleBodyContents").iterator();
-		if (content == null) {
-			content = conElement.select("div#newsEndContents").iterator();
+		Iterator<Element> content = null;
+		if (url.charAt(8) == n) {
+			Elements conElement = doc.select("div#main_content");
+			content = conElement.select("div#articleBodyContents").iterator();
+			if (content == null) {
+				content = conElement.select("div#newsEndContents").iterator();
+			}
+		}
+		if (url.charAt(8) == e) {
+			Elements conElement = doc.select("div.end_body_wrp");
+			content = conElement.select("div#articeBody").iterator(); // 연예 뉴스
+		}
+		if (url.charAt(8) == s) {
+			Elements conElement = doc.select("div.content_area");
+			content = conElement.select("div#newsEndContents").iterator(); // 스포츠 뉴스
 		}
 
-		
-		
-		while (content.hasNext()) {
-			System.out.println(content.next());
+		if (content.hasNext()) {
+			String con = content.next().text();
+			System.out.println(con);
+
 		}
 
 		System.out.println("------------------------");
+		// 본문 내용
+		/*
+		 * while (content.hasNext()) { System.out.println(content.next()); }
+		 */
 
+		System.out.println("------------------------");
+
+		// 댓글 갯수 구해서 넘겨야 하는데, 태그와 class명까지 같은거 추출하는 방법?
+		/*
+		 * Iterator<Element> count =
+		 * countElement.select("span.u_cbox_info_txt").iterator();
+		 * 
+		 * while(count.hasNext()) { System.out.println(count.next()); }
+		 */
 	}
 
 }
