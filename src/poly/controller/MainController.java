f67c65c646e499b70a4fd6fd3e5f5eb5d3da8175
bslovingu <sincethe1997@naver.com>
2020-06-08 09:32:18 +0900

    뉴스 url (네이버뉴스 존재하는 것만)가져오고 해당되는 기사에서 작성시간 가져오기

--- src/poly/controller/MainController.java
+++ src/poly/controller/MainController.java
@@ -1,5 +1,9 @@
 package poly.controller;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -7,9 +11,16 @@ import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import poly.dto.RankDTO;
+import poly.dto.ValueDTO;
+import poly.service.INaverRankService;
 import poly.service.IWeatherService;
+import poly.util.CmmUtil;
 
 @Controller
 public class MainController {
@@ -17,6 +28,9 @@ public class MainController {
 
 	@Resource(name = "WeatherService")
 	private IWeatherService weatherService;
+
+	@Resource(name = "NaverRankService")
+	private INaverRankService naverRankService;
 
 	// TTS페이지
 	@RequestMapping(value = "index")
@@ -35,8 +49,47 @@ public class MainController {
 
 	@RequestMapping(value = "googleTest")
 	public String result(HttpServletRequest request, Model model, HttpSession session) throws Exception {
-		
+
 		return "/googleTest";
+
+	}
+
+	@RequestMapping(value = "cssTestPage")
+	public String cssTestPage(HttpServletRequest request, Model model, HttpSession session) throws Exception {
+		log.info(this.getClass().getName() + " cssTestPage start!");
+		log.info(this.getClass().getName() + " cssTestPage end!");
+		return "/test/naverMainTest";
+
+	}
+
+	@RequestMapping(value = "cssTest", method = RequestMethod.POST)
+	@ResponseBody
+	public List<RankDTO> css(@RequestBody ValueDTO valueDto, Model model, HttpSession session) throws Exception {
+		log.info(this.getClass().getName() + " css start!");
+		List<RankDTO> rList = naverRankService.collectNaverRank(valueDto);
+		if(rList==null) {
+			rList = new ArrayList<RankDTO>();
+		}
+		log.info(this.getClass().getName() + " css end!");
+		return rList;
+
+	}
+	@RequestMapping(value = "relTest", method = RequestMethod.POST)
+	@ResponseBody
+	public List<String> relTest(HttpServletRequest request, Model model, HttpSession session) throws Exception {
+		log.info(this.getClass().getName() + " relTest start!");
+		
+		String keyword = CmmUtil.nvl(request.getParameter("keyword"));
+		
+		List<String> rList = new ArrayList<String>();
+		rList = naverRankService.getRelKeyword(keyword);
+		
+		if(rList==null) {
+			rList = new ArrayList<String>();
+		}
+		
+		log.info(this.getClass().getName() + " relTest end!");
+		return rList;
 
 	}
 }
--- src/test/NaverNewsTest.java
+++ src/test/NaverNewsTest.java
@@ -1,6 +1,9 @@
 package test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.json.simple.parser.ParseException;
 import org.jsoup.Jsoup;
@@ -13,33 +16,66 @@ public class NaverNewsTest {
 
 	public static void main(String[] args) throws IOException, ParseException {
 
-		String keyword = "경주";
+		String keyword = "홈플러스";
 		String urlEnc = UrlEncodeUtil.getEncode(keyword);
 		System.out.println(urlEnc);
-		
+
+		List<String> news_num = new ArrayList<String>();
+		Elements element1;
+		Iterator<Element> content = null;
+		String tmp;
+		String num;
+		String onclick;
 		String href;
-		String url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query="+urlEnc;
+		String url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=" + urlEnc;
 		// query 쪽 검색어 값을 url 인코더로 처리(encoding util 파일 하나 생성해서 구글 트렌드까지 해결하자)
-		// 실시간 검색어 설정을 엔터와 스포츠는 -2로 고정, 상위 세개 항목과 연령대만 버튼으로 지정
+		// 나이대 해결하기
 
 		Document doc = null;
 		doc = Jsoup.connect(url).get();
-
+		Iterator<Element> time = null;
 		// 위 페이지에서 기사 등록 시간 옆에 나오는 네이버뉴스 a태그 따라가면 네이버 뉴스로 통일된 기사에 접근가능
 		// 본문(본문쪽 content태그는 둘 중 하나, 대성이한테 받은거로 처리)과 댓글 쪽까지 접근가능 (본문으로는 워드클라우드, 댓글로는
 		// 오피니언 마이닝)
 
-		// 아래에 a href = "#"과 같이 잡혔는데 a[href^=http]로 처음부터 지정해서 a href = "http~~~~ "인 부분만 가져옴
+		// 아래에 a href = "#"과 같이 잡혔는데 a[href^=http]로 처음부터 지정해서 a href = "http~~~~ "인 부분만
+		// 가져옴
 		Elements element = doc.select("dd.txt_inline a[href^=http]");
-		
-		
+
 		for (Element a : element) {
 			href = a.attr("href");
+			onclick = a.attr("onclick");
+			int i = onclick.indexOf("&");
+			tmp = onclick.substring(36);
+			int j = tmp.indexOf("&");
+			num = tmp.substring(2, j);
+			news_num.add(num);
 			System.out.println(href);
+			/*
+			 * System.out.println(onclick); System.out.println(i); System.out.println(j);
+			 */
+			System.out.println(num);
+			System.out.println("--------------------");
 		}
 
+		System.out.println(news_num);
+		System.out.println(news_num.size());
 		System.out.println("------------------------");
 
+		for (int i = 0; i < news_num.size(); i++) {
+			element1 = doc.select("li#sp_nws" + news_num.get(i));
+			content = element1.select("dd.txt_inline").iterator();
+			String con = content.next().text();
+
+			int j = con.lastIndexOf("전");
+			String str_tmp = con.substring(0, j-1);
+			int k = str_tmp.lastIndexOf(" ");
+			String str_final = str_tmp.substring(k+1);
+			System.out.println(str_final);
+
+		}
+
+		System.out.println("----------------");
 	}
 
 }
